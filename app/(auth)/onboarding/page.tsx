d94e6c951350e525efa4edb6cb7f9b0526f5afb6
mo7g <MO7G+mr.mohammed1913@users.noreply.github.com>
2023-09-25 17:28:05 +0300

    the inboarding page is done frontend

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,28 @@
+import AccountPorfile from "@/components/forms/AccountProfile"
+import { currentUser } from "@clerk/nextjs"
+import { userInfo } from "os";
+
 async function Page() {
+    const user = await currentUser();
+    if (!user) return null;
+
+    const userData = {
+        id: user?.id,
+        objectId: user?._id,
+        username: user?.username || user?.username,
+        name: user?.name || user?.firstName || "",
+        bio: user?.bio || "",
+        image: user?.image || user.profileImageUrl
+    }
+
     return (
-        <main>
+        <main className="ma-auto flex max-w-3xl flex-col justify-start px-10 py-20">
             <h1 className="head-text">onBoarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">complete your profile now to use threads</p>
+
+            <section className="mt-10 bg-dark-2 p-10">
+                <AccountPorfile user={userData}/>
+            </section>
         </main>
     )
 }
