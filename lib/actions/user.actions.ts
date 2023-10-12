c7fdc1296f14797be58060b255e61dbe568c4bef
mo7g <no-replay@example.com>
2023-10-12 12:27:17 +0300

    dont

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,80 +1,76 @@
-"use server"
-import { connectToDB } from "../mongoose"
+"use server";
+import { connectToDB } from "../mongoose";
 import User from "../models/user.model";
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
+import { exec } from "child_process";
 
-interface Parms{
-    userId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-    path: string;
- }
+interface Parms {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
 // passing an object instead of a parameter is better !!
-// improved readablity and maintainability 
+// improved readablity and maintainability
 // avoiding argument order mistakes
-// flexability and extensibility 
-// named argument 
+// flexability and extensibility
+// named argument
 // destructing the object inside the function
-export async function updateUser(
-    { userId,
-        username,
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Parms): Promise<void> {
+  try {
+    connectToDB();
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
         name,
         bio,
         image,
-        path,
-    }: Parms ): Promise<void>{
-    try {
-         connectToDB();
-        await User.findOneAndUpdate(
-        { id: userId },
-        {
-            username: username.toLowerCase(),
-            name,
-            bio,
-            image,
-            onBoarded: true,
-        },
-        { upsert: true }
+        onBoarded: true,
+      },
+      { upsert: true }
     );
-    
-    if (path === '/profile/edit') {
-        // allows you to revalidate data associated with a specific path.
-        // useful when you want to update your cached data without waiting for a revalidation period  to expire
-        revalidatePath(path);
-        } 
-        
-    } catch (error:any) {
-        throw new Error(`Faild to create/update user: ${error.message}`)
-    }
-}
 
+    if (path === "/profile/edit") {
+      // allows you to revalidate data associated with a specific path.
+      // useful when you want to update your cached data without waiting for a revalidation period  to expire
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Faild to create/update user: ${error.message}`);
+  }
+}
 
 export async function fetchUser(userId: string) {
-    try {
-        connectToDB();
-        return await User
-            .findOne({ id: userId })
-        //     .populate({
-        //      path: 'communities',
-        //      model: Community
-        //   })
-    } catch (error:any) {
-        throw new Error(`Error from the fetchUser actioin ${error.message}`)
-    }
+  try {
+    connectToDB();
+    return await User.findOne({ id: userId });
+    //     .populate({
+    //      path: 'communities',
+    //      model: Community
+    //   })
+  } catch (error: any) {
+    throw new Error(`Error from the fetchUser actioin ${error.message}`);
+  }
 }
-
-
 
 export async function fetchUserPosts(userId: string) {
   try {
-        
     connectToDB();
 
-       // Find all threads authored by the user with the given userId
-     // Find all threads authored by the user with the given userId
+    // Find all threads authored by the user with the given userId
+    // Find all threads authored by the user with the given userId
     const threads = await User.findOne({ id: userId }).populate({
       path: "threads",
       model: Thread,
@@ -90,9 +86,53 @@ export async function fetchUserPosts(userId: string) {
         },
       ],
     });
-    return threads
-    
-    } catch (error : any) {
-        throw new Error(`Failed to fetch posts ${error.message}`)
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch posts ${error.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString: string;
+  pageNumber: number;
+  pageSize: number;
+  sortBy: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = { id: { $ne: userId } };
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
     }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users ${error.message}`);
+  }
 }
